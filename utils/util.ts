83b009da38cdfa9259182c81d662c96f2cef6c80
qiye <1710797241@qq.com>
2023-05-10 10:00:13 +0800

    fix: 版本低退出

--- utils/util.ts
+++ utils/util.ts
@@ -1,42 +1,51 @@
-
 export function compareVersion(v1Param: string, v2Param: string) {
-  const v1 = v1Param.split('.')
-  const v2 = v2Param.split('.')
-  const len = Math.max(v1.length, v2.length)
+    const v1 = v1Param.split('.');
+    const v2 = v2Param.split('.');
+    const len = Math.max(v1.length, v2.length);
 
-  while (v1.length < len) {
-    v1.push('0')
-  }
-  while (v2.length < len) {
-    v2.push('0')
-  }
-
-  for (let i = 0; i < len; i++) {
-    const num1 = parseInt(v1[i])
-    const num2 = parseInt(v2[i])
-
-    if (num1 > num2) {
-      return 1
-    } else if (num1 < num2) {
-      return -1
+    while (v1.length < len) {
+        v1.push('0');
     }
-  }
+    while (v2.length < len) {
+        v2.push('0');
+    }
 
-  return 0
+    for (let i = 0; i < len; i++) {
+        const num1 = parseInt(v1[i]);
+        const num2 = parseInt(v2[i]);
+
+        if (num1 > num2) {
+            return 1;
+        } else if (num1 < num2) {
+            return -1;
+        }
+    }
+
+    return 0;
 }
 
 export const showVersionTip = () => {
-  const version = wx.getAppBaseInfo().SDKVersion
+    const version = wx.getAppBaseInfo().SDKVersion;
 
-  if (compareVersion(version, '2.21.4') >= 0) {
-  //  版本之上
-  
-  } else {
-    // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提�?
-    wx.showModal({
-      title: '提示',
-      content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试�?
-    })
-  }
-
-}
+    if (compareVersion(version, '2.21.4') >= 0) {
+        //  版本之上
+    } else {
+        // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提�?
+        wx.showModal({
+            title: '提示',
+            content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。',
+            showCancel: false,
+            success(res) {
+                if (res.confirm) {
+                    console.log('用户点击确定');
+                    wx.exitMiniProgram({
+                        success: function () {},
+                        fail: function () {}
+                    });
+                } else if (res.cancel) {
+                    console.log('用户点击取消');
+                }
+            }
+        });
+    }
+};
